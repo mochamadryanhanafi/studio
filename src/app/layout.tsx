a481f6abfb625d3ef5bdd5d068101afb2de7079c
Mochamad Ryan Hanafi <mochamadryanhanafi@gmail.com>
2025-10-27 08:02:45 +0000

    <div class="sun"></div> <div class="sun2"></div> <div class="sun3"></div

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased relative overflow-x-hidden">
+        <div className="sun"></div>
+        <div className="sun2"></div>
+        <div className="sun3"></div>
         <LanguageProvider>
           <ThemeProvider
             attribute="class"
